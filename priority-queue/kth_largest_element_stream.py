4e12b8dec91a38e3c0fba158119a79fa89d7cff5
RajK158 <rajkundur.rk@gmail.com>
2026-07-05 18:21:43 -0700

    Heap/ Priority queue - Kth largest element stream

---
+++ priority-queue/kth_largest_element_stream.py
@@ -0,0 +1,41 @@
+# Problem: Kth Largest Element in a Stream
+# Pattern: Min Heap
+# Time:
+# constructor -> O(n log n)
+# add -> O(log k)
+# Space: O(k)
+
+from typing import List
+import heapq
+
+class KthLargest:
+
+    def __init__(self, k: int, nums: List[int]):
+
+        self.k = k
+        self.min_heap = nums
+
+        heapq.heapify(self.min_heap)
+
+        while len(self.min_heap) > k:
+            heapq.heappop(self.min_heap)
+
+    def add(self, val: int) -> int:
+
+        heapq.heappush(self.min_heap, val)
+
+        if len(self.min_heap) > self.k:
+            heapq.heappop(self.min_heap)
+
+        return self.min_heap[0]
+
+
+if __name__ == "__main__":
+
+    kthLargest = KthLargest(3, [1, 2, 3, 3])
+
+    print(kthLargest.add(3))  # 3
+    print(kthLargest.add(5))  # 3
+    print(kthLargest.add(6))  # 3
+    print(kthLargest.add(7))  # 5
+    print(kthLargest.add(8))  # 6
