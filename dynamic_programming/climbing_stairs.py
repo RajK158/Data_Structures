2adfae9a9d8c34195602eeca35295dd2c3141cc3
RajK158 <rajkundur.rk@gmail.com>
2026-08-21 15:39:16 -0700

    Dynamic Programming - Climbing Stairs

---
+++ dynamic_programming/climbing_stairs.py
@@ -0,0 +1,24 @@
+class Solution:
+
+    def climbStairs(self, n: int) -> int:
+
+        one = 1
+        two = 1
+
+        for _ in range(n - 1):
+
+            temp = one
+            one = one + two
+            two = temp
+
+        return one
+
+
+if __name__ == "__main__":
+
+    sol = Solution()
+
+    print(sol.climbStairs(2))   # 2
+    print(sol.climbStairs(3))   # 3
+    print(sol.climbStairs(5))   # 8
+    print(sol.climbStairs(10))  # 89
